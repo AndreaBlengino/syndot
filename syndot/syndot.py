8c29947b4d7831037c40202c377e8ba4455c62c0
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-04-17 21:00:43 +0200

    DEV: create init command parser

--- syndot/syndot.py
+++ syndot/syndot.py
@@ -4,4 +4,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 command_parser = parser.add_subparsers(dest = 'command')
 
+init_parser = command_parser.add_parser('init')
+init_parser.add_argument('-p', '--path', required = False)
+
 args = parser.parse_args()
