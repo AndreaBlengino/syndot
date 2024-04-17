053d7155ed92fdea8eb304a401cd40bb99b63869
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-04-17 21:18:47 +0200

    DEV: destination folder creation for init command

--- syndot/syndot.py
+++ syndot/syndot.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import os
 
 
 parser = ArgumentParser()
@@ -8,3 +9,10 @@ init_parser = command_parser.add_parser('init')
 init_parser.add_argument('-p', '--path', required = False)
 
 args = parser.parse_args()
+
+
+if args.command == 'init':
+    destination = args.path if args.path is not None else os.path.expanduser('~/Settings')
+    if os.path.exists(destination):
+        raise ValueError(f"Destination folder {destination} already exists.")
+    os.mkdir(destination)
