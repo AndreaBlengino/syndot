0ce7a28bc866b611cc5148f29e4609309956b086
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-04-17 21:37:26 +0200

    ENH: make destination folder reading more robust

--- syndot/syndot.py
+++ syndot/syndot.py
@@ -13,9 +13,9 @@ args = parser.parse_args()
 
 
 if args.command == 'init':
-    destination = args.path if args.path is not None else os.path.expanduser('~/Settings')
+    destination = os.path.expanduser(args.path if args.path is not None else '~/Settings')
     if os.path.exists(destination):
-        raise ValueError(f"Destination folder {destination} already exists.")
+        raise ValueError(f"Destination directory {destination} already exists.")
     os.mkdir(destination)
 
     config = ConfigParser()
