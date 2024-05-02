1e6560f9d1c90cd77b0e428b22a47b2daf14e4bc
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-05-02 23:38:25 +0200

    ENH: improve map file template creation

--- syndot/commands/init.py
+++ syndot/commands/init.py
@@ -1,4 +1,5 @@
 from argparse import Namespace
+from configparser import ConfigParser
 import os
 from syndot import utils
 
@@ -8,7 +9,7 @@ MAP_TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), '_t
 
 
 def init(args: Namespace) -> None:
-    settings_dir = os.path.expanduser(args.path if args.path is not None else DEFAULT_SETTINGS_DIR)
+    settings_dir = utils.expand_home_path(args.path if args.path is not None else DEFAULT_SETTINGS_DIR)
     if os.path.exists(settings_dir):
         raise ValueError(f"Settings directory {settings_dir} already exists.")
     os.mkdir(settings_dir)
@@ -16,4 +17,14 @@ def init(args: Namespace) -> None:
     config = utils.read_map_file(MAP_TEMPLATE_PATH)
     config['Path']['settings_dir'] = settings_dir
 
+    expand_default_target_path(config = config, target_category = 'files')
+    expand_default_target_path(config = config, target_category = 'directories')
+
     utils.write_map_file(map_file_path = os.path.join(settings_dir, 'map.ini'), config = config)
+
+
+def expand_default_target_path(config: ConfigParser, target_category: str):
+    targets = [utils.expand_home_path(target) for target in config['Targets'][target_category].split()]
+    targets = list(set(targets))
+    targets.sort()
+    config['Targets'][target_category] = '\n' + '\n'.join(targets)
