64cf726f6352345ca508a998df61a5df4784d1e4
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-05-17 20:41:56 +0200

    TST: add all utils.path tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,24 @@ import os
 
 
 TEST_DATA_PATH = 'test_data'
+SETTINGS_DIR = os.path.join(TEST_DATA_PATH, 'Settings')
 
 
 @composite
-def paths(draw):
+def paths(draw, absolute = False):
     folder_list = draw(lists(elements = text(min_size = 5, max_size = 10, alphabet = characters(min_codepoint = 97,
                                                                                                 max_codepoint = 122)),
                              min_size = 2,
                              max_size = 5))
-    folder_list.insert(0, os.path.join('.', TEST_DATA_PATH))
+    if absolute:
+        folder_list.insert(0, os.path.join(os.getcwd(), TEST_DATA_PATH))
+    else:
+        folder_list.insert(0, os.path.join('.', TEST_DATA_PATH))
 
     return os.path.join(*folder_list)
+
+
+@composite
+def usernames(draw):
+    return draw(text(min_size = 5, max_size = 10, alphabet = characters(min_codepoint = 97, max_codepoint = 122)))
+
--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -1,10 +1,10 @@
 from hypothesis import given, settings
-from hypothesis.strategies import booleans
+from hypothesis.strategies import booleans, one_of, none
 import os
 from pytest import mark
 import shutil
-from syndot.utils.path import generate_backup_path
-from tests.conftest import paths, TEST_DATA_PATH
+from syndot.utils.path import generate_backup_path, split_path, expand_home_path, compose_target_paths
+from tests.conftest import paths, usernames, TEST_DATA_PATH, SETTINGS_DIR
 
 
 @mark.utils
@@ -12,7 +12,7 @@ class TestGenerateBackupPath:
 
     @mark.genuine
     @given(path = paths(), is_file = booleans())
-    @settings(max_examples = 10, deadline = None)
+    @settings(max_examples = 100, deadline = None)
     def test_function(self, path, is_file):
         if is_file:
             os.makedirs(os.path.dirname(path))
@@ -23,6 +23,8 @@ class TestGenerateBackupPath:
 
         backup_path = generate_backup_path(path = path)
 
+        assert isinstance(backup_path, str)
+        assert backup_path
         if is_file:
             assert backup_path.endswith('.bak')
         else:
@@ -30,3 +32,66 @@ class TestGenerateBackupPath:
         assert path == backup_path[:-4]
 
         shutil.rmtree(TEST_DATA_PATH)
+
+
+@mark.utils
+class TestSplitPath:
+
+    @mark.genuine
+    @given(path = paths())
+    @settings(max_examples = 100, deadline = None)
+    def test_function(self, path):
+        splitted = split_path(path = path)
+
+        assert isinstance(splitted, list)
+        assert splitted
+        assert len(splitted) == path.count(os.sep)
+        assert path.endswith(os.sep.join(splitted))
+
+
+@mark.utils
+class TestExpandHomePath:
+
+    @mark.genuine
+    @given(path = paths(), sudo_user = one_of(usernames(), none()), add_home = booleans())
+    @settings(max_examples = 100, deadline = None)
+    def test_function(self, path, sudo_user, add_home):
+        if sudo_user:
+            os.environ['SUDO_USER'] = sudo_user
+        if add_home:
+            path = os.path.join('~', path)
+
+        expanded_path = expand_home_path(path = path)
+
+        assert isinstance(expanded_path, str)
+        assert expanded_path
+        if add_home:
+            if sudo_user:
+                assert expanded_path.startswith(os.path.join(os.sep, 'home', sudo_user))
+            else:
+                assert expanded_path.startswith(os.path.join(os.sep, 'home'))
+        else:
+            assert expanded_path == path
+
+
+class TestComposeTargetPaths:
+
+    @mark.genuine
+    @given(target_path = paths(absolute = True), add_home = booleans())
+    @settings(max_examples = 100, deadline = None)
+    def test_function(self, target_path, add_home):
+        if add_home:
+            target_path = os.path.join('~', target_path)
+
+        system_target_path, settings_target_path = compose_target_paths(settings_dir = SETTINGS_DIR,
+                                                                        target = target_path)
+
+        for path in [system_target_path, settings_target_path]:
+            assert isinstance(path, str)
+            assert path
+        if add_home:
+            assert system_target_path.startswith(os.path.join(os.sep, 'home'))
+        else:
+            assert system_target_path == target_path
+        assert settings_target_path.startswith(SETTINGS_DIR)
+        assert settings_target_path.endswith(target_path)
