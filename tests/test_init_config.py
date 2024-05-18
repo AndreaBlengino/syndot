c47dc20a9d38888042646f188d623013f43861d7
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-05-19 00:44:45 +0200

    TST: add init_config tests

---
+++ tests/test_init_config.py
@@ -0,0 +1,40 @@
+import os
+from pytest import mark
+import shutil
+from syndot import init_config
+from tests.conftest import TEST_DATA_PATH
+
+
+@mark.config
+class TestInitConfig:
+
+    @mark.genuine
+    def test_function(self, monkeypatch):
+
+        def mock_config_path():
+            return os.path.join(TEST_DATA_PATH, '.config', 'syndot')
+
+        def mock_log_path():
+            return os.path.join(TEST_DATA_PATH, '.config', 'syndot', 'log_file.log')
+
+        config_path = mock_config_path()
+        log_file_path = mock_log_path()
+
+        monkeypatch.setattr(init_config, 'CONFIG_DIR_PATH', config_path)
+        monkeypatch.setattr(init_config, 'LOG_FILE_PATH', log_file_path)
+
+        assert not os.path.exists(config_path)
+
+        init_config.init_config()
+
+        assert os.path.exists(config_path)
+        assert os.path.isdir(config_path)
+        for asset in ['templates', 'colorschemes']:
+            asset_path = os.path.join(config_path, asset)
+            assert os.path.exists(asset_path)
+            assert os.path.isdir(asset_path)
+            assert os.listdir(asset_path)
+        assert os.path.exists(log_file_path)
+        assert os.path.isfile(log_file_path)
+
+        shutil.rmtree(config_path)
