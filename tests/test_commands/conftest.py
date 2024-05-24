845a8820bfd90d20ae8c371bacf74c861e255127
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-05-24 20:59:38 +0200

    TST: add unlink tests

--- tests/test_commands/conftest.py
+++ tests/test_commands/conftest.py
@@ -47,7 +47,7 @@ def create_parent_directory(path: str) -> None:
         os.makedirs(parent_path)
 
 
-def generate_testing_system_files(status: str) -> None:
+def generate_link_testing_system_files(status: str) -> None:
     config = ConfigParser()
     config.read(TEST_MAP_FILE_PATH)
 
@@ -58,8 +58,6 @@ def generate_testing_system_files(status: str) -> None:
         for target_list, is_file in zip([target_files, target_directories], [True, False]):
             for target in target_list:
                 create_file_or_directory(path = target, is_file = is_file)
-        for directory in target_directories:
-            create_file_or_directory(path = directory, is_file = False)
     elif status == 'already_existing_settings':
         for target_list, is_file in zip([target_files, target_directories], [True, False]):
             for target in target_list:
@@ -88,3 +86,50 @@ def generate_testing_system_files(status: str) -> None:
                 create_file_or_directory(path = settings_target_path, is_file = is_file)
                 create_parent_directory(path = target)
                 os.symlink(os.path.join(settings_target_path + '.other'), target)
+
+
+def generate_unlink_testing_system_files(status: str) -> None:
+    config = ConfigParser()
+    config.read(TEST_MAP_FILE_PATH)
+
+    target_directories = config['Targets']['directories'].split()
+    target_files = config['Targets']['files'].split()
+
+    if status == 'targets_to_be_unlinked':
+        for target_list, is_file in zip([target_files, target_directories], [True, False]):
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                create_file_or_directory(path = settings_target_path, is_file = is_file)
+                create_parent_directory(path = target)
+                os.symlink(settings_target_path, target)
+    elif status == 'wrong_existing_links':
+        for target_list, is_file in zip([target_files, target_directories], [True, False]):
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                create_file_or_directory(path = settings_target_path, is_file = is_file)
+                create_parent_directory(path = target)
+                os.symlink(os.path.join(settings_target_path + '.other'), target)
+    elif status == 'missing_system_targets':
+        for target_list, is_file in zip([target_files, target_directories], [True, False]):
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                create_file_or_directory(path = settings_target_path, is_file = is_file)
+    elif status == 'already_existing_system':
+        for target_list, is_file in zip([target_files, target_directories], [True, False]):
+            for target in target_list:
+                settings_target_path = os.path.join(SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                create_file_or_directory(path = target, is_file = is_file)
+                create_file_or_directory(path = settings_target_path, is_file = is_file)
+    elif status == 'already_unlinked_targets':
+        for target_list, is_file in zip([target_files, target_directories], [True, False]):
+            for target in target_list:
+                create_file_or_directory(path = target, is_file = is_file)
+    elif status == 'missing_settings_targets':
+        return
+    elif status == 'settings_are_links':
+        for target_list, is_file in zip([target_files, target_directories], [True, False]):
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                other_target = settings_target_path + '.other'
+                create_file_or_directory(path = other_target, is_file = is_file)
+                os.symlink(other_target, settings_target_path)
--- tests/test_commands/test_unlink.py
+++ tests/test_commands/test_unlink.py
@@ -4,27 +4,27 @@ from hypothesis.strategies import one_of, none, booleans, sampled_from
 import os
 from pytest import mark
 import shutil
-from syndot.commands import link
+from syndot.commands import unlink
 from syndot.utils import prompt
 from syndot.utils.prompt import VALID_PROMPT_CHOICES
 from tests.conftest import targets, valid_targets, TEST_DATA_PATH, SETTINGS_DIR
-from tests.test_commands.conftest import generate_link_testing_system_files, generate_testing_map_file, TEST_MAP_FILE_PATH
+from tests.test_commands.conftest import generate_unlink_testing_system_files, generate_testing_map_file, TEST_MAP_FILE_PATH
 
 
 @mark.commands
-class TestLink:
+class TestUnlink:
 
     @mark.genuine
     @given(exact_match = booleans(),
            target_path = one_of(targets(absolute = False), none()),
            truncate_path = booleans(),
-           backup = booleans(),
            answer = sampled_from(elements = [*VALID_PROMPT_CHOICES.keys(), '']),
-           target_status = sampled_from(elements = ['targets_to_be_linked', 'already_existing_settings',
-                                                    'missing_system_targets', 'already_linked_targets',
-                                                    'corrupted_targets', 'wrong_existing_links']))
+           target_status = sampled_from(elements = ['targets_to_be_unlinked', 'wrong_existing_links',
+                                                    'missing_system_targets', 'already_existing_system',
+                                                    'already_unlinked_targets', 'missing_settings_targets',
+                                                    'settings_are_links']))
     @settings(max_examples = 100, deadline = None)
-    def test_function(self, exact_match, target_path, truncate_path, backup, answer, target_status):
+    def test_function(self, exact_match, target_path, truncate_path, answer, target_status):
 
         args = Namespace()
         args.mapfile = TEST_MAP_FILE_PATH
@@ -32,10 +32,9 @@ class TestLink:
         args.exact = exact_match
         truncated_target_path = os.path.split(target_path)[0] if truncate_path and target_path is not None else target_path
         args.TARGET_PATH_START = truncated_target_path
-        args.backup = backup
 
         generate_testing_map_file()
-        generate_link_testing_system_files(status = target_status)
+        generate_unlink_testing_system_files(status = target_status)
 
         target_list = [target.replace('~', os.path.join(os.getcwd(), TEST_DATA_PATH)) for target in valid_targets]
         if target_path:
@@ -43,108 +42,112 @@ class TestLink:
                 target_list = [target for target in target_list if target == truncated_target_path]
             else:
                 target_list = [target for target in target_list if target.startswith(truncated_target_path)]
-        if target_status == 'targets_to_be_linked':
-            for target in target_list:
-                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert os.path.exists(target)
-                assert not os.path.islink(target)
-                assert not os.path.exists(settings_target_path)
-        elif target_status == 'already_existing_settings':
-            for target in target_list:
-                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert os.path.exists(target)
-                assert not os.path.islink(target)
-                assert os.path.exists(settings_target_path)
-        elif target_status == 'missing_system_targets':
-            for target in target_list:
-                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert not os.path.exists(target)
-                assert not os.path.exists(settings_target_path)
-        elif target_status == 'already_linked_targets':
+        if target_status == 'targets_to_be_unlinked':
             for target in target_list:
                 settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
                 assert os.path.islink(target)
                 assert os.readlink(target) == settings_target_path
                 assert os.path.exists(settings_target_path)
-        elif target_status == 'corrupted_targets':
-            for target in target_list:
-                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert os.path.islink(target)
-                assert os.readlink(target) == settings_target_path
-                assert not os.path.exists(settings_target_path)
         elif target_status == 'wrong_existing_links':
             for target in target_list:
                 settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
                 assert os.path.islink(target)
                 assert os.readlink(target) != settings_target_path
+        elif target_status == 'missing_system_targets':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                assert not os.path.exists(target)
+                assert os.path.exists(settings_target_path)
+        elif target_status == 'already_existing_system':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                assert os.path.exists(target)
+                assert not os.path.islink(target)
+                assert os.path.exists(settings_target_path)
+        elif target_status == 'already_unlinked_targets':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                assert os.path.exists(target)
+                assert not os.path.islink(target)
+                assert not os.path.exists(settings_target_path)
+        elif target_status == 'missing_settings_targets':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                assert not os.path.exists(target)
+                assert not os.path.exists(settings_target_path)
+        elif target_status == 'settings_are_links':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                assert os.path.islink(settings_target_path)
 
         prompt.input = lambda x: answer
-        link(args = args)
+        unlink(args = args)
 
-        if target_status == 'targets_to_be_linked':
+        if target_status == 'targets_to_be_unlinked':
             for target in target_list:
                 settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
                 if answer == '':
                     answer = 'n'
                 if VALID_PROMPT_CHOICES[answer]:
-                    assert os.path.islink(target)
-                    assert os.readlink(target) == settings_target_path
-                    assert os.path.exists(settings_target_path)
-                    if backup:
-                        if os.path.isfile(settings_target_path):
-                            backup_path = target + '.bak'
-                            assert os.path.exists(backup_path)
-                            assert os.path.isfile(backup_path)
-                        elif os.path.isdir(settings_target_path):
-                            backup_path = target + '_bak'
-                            assert os.path.isdir(backup_path)
-                else:
                     assert os.path.exists(target)
                     assert not os.path.islink(target)
                     assert not os.path.exists(settings_target_path)
-        elif target_status == 'already_existing_settings':
+                else:
+                    assert os.path.islink(target)
+                    assert os.readlink(target) == settings_target_path
+                    assert os.path.exists(settings_target_path)
+        elif target_status == 'wrong_existing_links':
             for target in target_list:
                 settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
                 if answer == '':
                     answer = 'n'
                 if VALID_PROMPT_CHOICES[answer]:
+                    assert os.path.exists(target)
+                    assert not os.path.islink(target)
+                    assert not os.path.exists(settings_target_path)
+                else:
                     assert os.path.islink(target)
-                    assert os.readlink(target) == settings_target_path
+                    assert os.readlink(target) != settings_target_path
+        elif target_status == 'missing_system_targets':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                if answer == '':
+                    answer = 'n'
+                if VALID_PROMPT_CHOICES[answer]:
+                    assert os.path.exists(target)
+                    assert not os.path.islink(target)
+                    assert not os.path.exists(settings_target_path)
+                else:
+                    assert not os.path.exists(target)
                     assert os.path.exists(settings_target_path)
-                    if backup:
-                        if os.path.isfile(settings_target_path):
-                            backup_path = target + '.bak'
-                            assert os.path.exists(backup_path)
-                            assert os.path.isfile(backup_path)
-                        elif os.path.isdir(settings_target_path):
-                            backup_path = target + '_bak'
-                            assert os.path.isdir(backup_path)
+        elif target_status == 'already_existing_system':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                if answer == '':
+                    answer = 'n'
+                if VALID_PROMPT_CHOICES[answer]:
+                    assert os.path.exists(target)
+                    assert not os.path.islink(target)
+                    assert not os.path.exists(settings_target_path)
                 else:
                     assert os.path.exists(target)
                     assert not os.path.islink(target)
                     assert os.path.exists(settings_target_path)
-        elif target_status == 'missing_system_targets':
+        elif target_status == 'already_unlinked_targets':
+            for target in target_list:
+                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
+                assert os.path.exists(target)
+                assert not os.path.islink(target)
+                assert not os.path.exists(settings_target_path)
+        elif target_status == 'missing_settings_targets':
             for target in target_list:
                 settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
                 assert not os.path.exists(target)
                 assert not os.path.exists(settings_target_path)
-        elif target_status == 'already_linked_targets':
+        elif target_status == 'settings_are_links':
             for target in target_list:
                 settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert os.path.islink(target)
-                assert os.readlink(target) == settings_target_path
-                assert os.path.exists(settings_target_path)
-        elif target_status == 'corrupted_targets':
-            for target in target_list:
-                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert os.path.islink(target)
-                assert os.readlink(target) == settings_target_path
-                assert not os.path.exists(settings_target_path)
-        elif target_status == 'wrong_existing_links':
-            for target in target_list:
-                settings_target_path = os.path.join(os.getcwd(), SETTINGS_DIR, *target.split(os.path.sep)[1:])
-                assert os.path.islink(target)
-                assert os.readlink(target) != settings_target_path
+                assert os.path.islink(settings_target_path)
 
         shutil.rmtree(TEST_DATA_PATH)
 
