7ea69e9638179a4a5b92df224b68b0d0bc80f0d8
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-06-01 18:16:15 +0200

    REL: update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
+import subprocess
 
 
-setup(version = "v0.0.0")
+version = subprocess.run(['git', 'describe', '--tags'], stdout = subprocess.PIPE).stdout.decode('utf-8').strip()
+
+
+try:
+    setup(version = version)
+except Exception as e:
+    if e.args[0].startswith("Invalid version: "):
+        setup(version = 'v0.0.0')
+    else:
+        print(e)
