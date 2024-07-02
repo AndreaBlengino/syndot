b4126cfc576718043293d87ba76c8b239aa763e6
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-07-02 20:36:52 +0200

    DEV: update parser in order to handle labels
    Introduced label-grouping mechanism of the paths in the map file

--- syndot/__main__.py
+++ syndot/__main__.py
@@ -1,10 +1,15 @@
 from syndot import commands
 from syndot.parser.parser import parser
+from syndot.parser.remove_parser import remove_parser
 from syndot import init_config
 
 
 init_config.init_config()
 args = parser.parse_args()
+
+if args.command == 'remove' and args.label is None and args.path is None:
+    remove_parser.error("At least a [-l | --label] or a [-p | --path] must be"
+                        "specified")
 
 command_map = {'init': commands.init,
                'link': commands.link,
--- syndot/parser/remove_parser.py
+++ syndot/parser/remove_parser.py
@@ -5,25 +5,42 @@ from syndot.parser.parser import command_parser, CommandFormatter
 remove_parser = command_parser.add_parser(
     'remove',
     prog='syndot remove',
-    usage='%(prog)s [OPTION] ARGUMENT',
+    usage='%(prog)s ([[-l | --label] <LABEL>...] | [[-p | --path] <PATH>...]) '
+          '[[-m | --mapfile] <MAP_FILE>]',
     description='Remove dotfiles from the map file',
     help='Remove dotfiles from the map file',
     add_help=False,
     formatter_class=CommandFormatter)
-remove_parser._positionals.title = 'ARGUMENTS'
-remove_parser._optionals.title = 'OPTIONS'
-remove_parser.add_argument(
-    'TARGET_PATH',
-    help='Dotfile path')
+
 remove_parser.add_argument(
     '-h', '--help',
     action='help',
     default=SUPPRESS,
     help='Show this help message and exit')
+
+remove_parser.add_argument(
+    '-l', '--label',
+    required=False,
+    nargs='+',
+    dest='label',
+    metavar='<LABEL>',
+    help='Label(s) and relative path(s) to remove from the map file. One or '
+         'more labels can be provided. At least a <LABEL> or a <PATH> must be '
+         'provided')
+
 remove_parser.add_argument(
     '-m', '--mapfile',
     required=False,
     metavar='<MAP_FILE>',
     help='Path to the %(metavar)s. If not provided search for a \'map.ini\' '
          'file in the current directory, so not required if the current '
-         'directory is the <SETTINGS> directory')
+         'directory is the settings directory')
+
+remove_parser.add_argument(
+    '-p', '--path',
+    required=False,
+    nargs='+',
+    dest='path',
+    metavar='<PATH>',
+    help='Dotfile path(s) to remove from the map file. One of more paths can '
+         'be provided. At least a <LABEL> or a <PATH> must be provided')
