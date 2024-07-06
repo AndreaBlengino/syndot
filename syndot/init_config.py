f432b70c44d9467e596b72d07aea1c3815155751
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-07-06 14:37:05 +0200

    DEV: move log file to ~/.local/share directory

--- syndot/init_config.py
+++ syndot/init_config.py
@@ -6,7 +6,8 @@ from syndot.utils.file_actions import copy
 SOURCE_PATH = os.path.dirname(__file__)
 HOME_PATH = expand_home_path('~')
 CONFIG_DIR_PATH = os.path.join(HOME_PATH, '.config', 'syndot')
-LOG_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'log_file.log')
+LOG_FILE_PATH = os.path.join(
+    HOME_PATH, '.local', 'share', 'syndot', 'log_file.log')
 
 
 def copy_sources(source: str, destination: str) -> None:
@@ -17,9 +18,10 @@ def copy_sources(source: str, destination: str) -> None:
 
 def init_config() -> None:
     if not os.path.exists(CONFIG_DIR_PATH):
-
         copy_sources(source='_templates', destination='templates')
         copy_sources(source='_colorschemes', destination='colorschemes')
 
+    if not os.path.exists(LOG_FILE_PATH):
+        os.makedirs(os.path.dirname(LOG_FILE_PATH))
         with open(LOG_FILE_PATH, 'w') as log_file:
             log_file.write('')
