97c0f25ac344e550ea5fd52acba0d913826d9c01
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-07-31 20:55:27 +0200

    TST: update list test according to search option

--- tests/test_commands/test_list.py
+++ tests/test_commands/test_list.py
@@ -2,9 +2,9 @@ from argparse import Namespace
 from configparser import ConfigParser
 from hypothesis import given, settings, HealthCheck
 from hypothesis.strategies import booleans
-from pytest import mark
+from pytest import mark, raises
 from syndot.commands.list_ import list_
-from tests.conftest import reset_environment
+from tests.conftest import reset_environment, labels
 from tests.test_commands.conftest import (
     generate_testing_map_file,
     TEST_MAP_FILE_PATH
@@ -18,7 +18,8 @@ class TestList:
     @given(
         directory_arg=booleans(),
         label_arg=booleans(),
-        path_arg=booleans()
+        path_arg=booleans(),
+        search=labels()
     )
     @settings(
         max_examples=100,
@@ -30,6 +31,7 @@ class TestList:
         directory_arg,
         label_arg,
         path_arg,
+        search,
         capsys
     ):
         reset_environment()
@@ -40,16 +42,20 @@ class TestList:
             if path_arg:
                 args.label = False
                 args.path = False
+                args.search = search
             else:
                 args.label = True
                 args.path = False
+                args.search = None
         else:
             if path_arg:
                 args.label = False
                 args.path = True
+                args.search = None
             else:
                 args.label = False
                 args.path = False
+                args.search = None
         args.mapfile = TEST_MAP_FILE_PATH
         generate_testing_map_file()
 
@@ -66,6 +72,7 @@ class TestList:
         path_list = []
         for path in config['Targets'].values():
             path_list.extend(path.split())
+        search_path_list = config['Targets'][search].split()
 
         if directory_arg:
             assert "Settings directory:" in printed_output
@@ -78,12 +85,35 @@ class TestList:
             if not args.path:
                 assert label in printed_output
             else:
-                assert label not in printed_output
+                if args.label and search == label:
+                    assert label in printed_output
+                else:
+                    assert label not in printed_output
 
         for path in path_list:
             if not args.label:
                 assert path in printed_output
             else:
-                assert path not in printed_output
+                if args.path and path in search_path_list:
+                    assert path in printed_output
+                else:
+                    assert path not in printed_output
+
+        reset_environment()
+
+    @mark.error
+    def test_raises_name_error(self):
+        reset_environment()
+
+        args = Namespace()
+        args.directory = False
+        args.label = False
+        args.path = False
+        args.search = 'label_not_in_map_file'
+        args.mapfile = TEST_MAP_FILE_PATH
+        generate_testing_map_file()
+
+        with raises(NameError):
+            list_(args=args)
 
         reset_environment()
