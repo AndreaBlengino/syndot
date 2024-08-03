11ce8d324e04a3e88978ec69c8da53fc0961d2f8
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-08-03 23:23:48 +0200

    TST: add print dotfiles function test

--- tests/test_utils/test_commands.py
+++ tests/test_utils/test_commands.py
@@ -1,11 +1,12 @@
 from hypothesis import given, settings, HealthCheck
-from hypothesis.strategies import lists, text, characters
+from hypothesis.strategies import lists, text, characters, dictionaries
 from pytest import mark
-from syndot.utils.commands import skip_dotfiles
+from syndot.utils.commands import skip_dotfiles, print_dotfiles_to_manage
 from tests.conftest import targets
 
 
 @mark.utils
+@mark.wip
 class TestSkipDotfiles:
 
     @mark.genuine
@@ -60,3 +61,80 @@ class TestSkipDotfiles:
         else:
             assert ((single_file_sentence in lines[-1]) or
                     (single_directory_sentence in lines[-1]))
+
+
+@mark.utils
+@mark.wip
+class TestPrintDotfilesToManage:
+
+    @mark.genuine
+    @given(
+        targets_list=dictionaries(
+            keys=text(
+                min_size=5,
+                max_size=10,
+                alphabet=characters(min_codepoint=97, max_codepoint=122)
+            ),
+            values=text(
+                min_size=5,
+                max_size=10,
+                alphabet=characters(min_codepoint=97, max_codepoint=122)
+            ),
+            min_size=1,
+            max_size=3
+        ),
+        many_targets_sentence=text(
+            min_size=5,
+            max_size=10,
+            alphabet=characters(min_codepoint=97, max_codepoint=122)
+        ),
+        single_file_sentence=text(
+            min_size=5,
+            max_size=10,
+            alphabet=characters(min_codepoint=97, max_codepoint=122)
+        ),
+        single_directory_sentence=text(
+            min_size=5,
+            max_size=10,
+            alphabet=characters(min_codepoint=97, max_codepoint=122)
+        ),
+        symbol=text(
+            min_size=5,
+            max_size=10,
+            alphabet=characters(min_codepoint=97, max_codepoint=122)
+        )
+    )
+    @settings(
+        max_examples=100,
+        deadline=None,
+        suppress_health_check=[HealthCheck.function_scoped_fixture]
+    )
+    def test_function(
+        self,
+        targets_list,
+        many_targets_sentence,
+        single_file_sentence,
+        single_directory_sentence,
+        symbol,
+        capsys
+    ):
+        print_dotfiles_to_manage(
+            targets_list=targets_list,
+            many_targets_sentence=many_targets_sentence,
+            single_file_sentence=single_file_sentence,
+            single_directory_sentence=single_directory_sentence,
+            symbol=symbol
+        )
+        printed_output = capsys.readouterr().out
+
+        assert isinstance(printed_output, str)
+        assert printed_output
+        for source, destination in targets_list.items():
+            assert source in printed_output
+            assert destination in printed_output
+        assert symbol in printed_output
+        if len(targets_list.keys()) == 1:
+            assert (single_file_sentence in printed_output) or \
+                   (single_directory_sentence in printed_output)
+        else:
+            assert many_targets_sentence in printed_output
